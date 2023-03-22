36f604b603008f8d190bf4d6e6e8b24630672993
eladjmc <eladjmc88@gmail.com>
2023-03-22 16:16:47 +0200

    updated the task up to 20.1

--- controller/productController.js
+++ controller/productController.js
@@ -57,13 +57,98 @@ export const getProducts = async (req, res, next) => {
     });
   }
 };
+
+export const deleteAllProducts = async (req, res, next) => {
+  try {
+    const product = await Product.deleteMany({});
+    res.status(200).json({
+      success: true,
+      data: product,
+    });
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      error: error.message,
+    });
+  }
+};
+
 export const getProductById = async (req, res, next) => {
   try {
     const id = req.params.id;
     const product = await Product.findById(id);
+    if (!product) {
+      throw new Error("No such product in database");
+    }
     res.status(200).json({
       success: true,
       data: product,
+    });
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      error: error.message,
+    });
+  }
+};
+
+export const updateProduct = async (req, res, next) => {
+  try {
+    console.log(typeof req.body.isActive !== 'boolean', typeof req.body.details.discount !== 'number' );
+    const id = req.params.id;
+    if (
+        typeof req.body.isActive !== 'boolean' &&
+        typeof req.body.details.discount !== 'number'
+    ) {
+      throw new Error(
+        "Active and Discount where not provided, cannot edit other fields"
+      );
+    }
+
+    const { isActive, details } = { ...req.body };
+    const { discount } = { ...details };
+
+    const updatedFields = {};
+
+    if (typeof isActive === 'boolean') {
+      updatedFields.isActive = isActive;
+    }
+    if (typeof discount === 'number') {
+        updatedFields['details.discount'] = discount;
+    }
+
+    const product = await Product.updateOne(
+      { _id: id },
+      { $set: updatedFields }
+    );
+
+    if (!product) {
+      throw new Error("No such product in database");
+    }
+
+    res.status(200).json({
+      success: true,
+      data: "Item Was updated successfully!",
+    });
+
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      error: error.message,
+    });
+  }
+};
+
+export const deleteProduct = async (req, res, next) => {
+  try {
+    const id = req.params.id;
+    const product = await Product.findByIdAndRemove(id);
+    if (!product) {
+      throw new Error("No such product in database");
+    }
+    res.status(200).json({
+      success: true,
+      data: `Product with id of ${id} was removed`,
     });
   } catch (error) {
     res.status(400).json({
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -4,13 +4,16 @@ import {
   getProducts,
   getProductById,
   getActiveProducts,
+  deleteProduct,
+  deleteAllProducts,
+  updateProduct,
 } from "../controller/productController.js";
 const router = express.Router();
 
-router.route("/").get(getProducts).post(addProduct);
+router.route("/").get(getProducts).post(addProduct).delete(deleteAllProducts);
 
 router.route("/active").get(getActiveProducts);
 
-router.route("/:id").get(getProductById);
+router.route("/:id").get(getProductById).delete(deleteProduct).patch(updateProduct);
 
 export default router;
